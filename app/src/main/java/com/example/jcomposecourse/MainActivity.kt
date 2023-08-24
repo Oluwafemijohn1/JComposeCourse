cd3144d13aa89d5b1d9c9476a3e708b41f3746c1
Oluwafemijohn1 <femiogundipe01@gmail.com>
2023-08-24 06:39:25 +0100

    Learnt spacer and weight

---
+++ app/src/main/java/com/example/jcomposecourse/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.jcomposecourse
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.jcomposecourse.ui.theme.JComposeCourseTheme
+import com.example.jcomposecourse.ui.screens.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JComposeCourseTheme {
+                // A surface container using the 'background' color from the theme
+                HomeScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JComposeCourseTheme {
+        Greeting("Android")
+    }
+}
